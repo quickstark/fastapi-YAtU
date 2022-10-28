d220f95cf4ca11e564e404c195a3bf45155dac7a
quickstark <dirk@quickstark.com>
2022-10-28 13:47:15 -0500

    Initial Image Endpoint

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
+from datetime import date
+from typing import List, Optional
+import os
+from dotenv import load_dotenv
+import psycopg2
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(debug=True)
+
+load_dotenv()
+DB = os.getenv('PGDATABASE')
+HOST= os.getenv('PGHOST')
+PORT = os.getenv('PGPORT')
+USER = os.getenv('PGUSER')
+PW = os.getenv('PGPASSWORD')
 
 class Msg(BaseModel):
     msg: str
 
+class ImageModel(BaseModel):
+    id: int
+    name: str
+    width: Optional[int]
+    height: Optional[int]
+    url: Optional[str]
+    url_resize: Optional[str]
+    date_added: Optional[date]
+    date_identified: Optional[date]
+
+@app.get("/images", response_model=List[ImageModel])
+async def get_all_images():
+    #Connect to Postgres
+    conn = psycopg2.connect(
+        database=DB, user=USER, password=PW, host=HOST, port=PORT
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM images ORDER BY id DESC")
+    rows = cur.fetchall()
+
+    formatted_photos = []
+    for row in rows:
+        formatted_photos.append(
+            ImageModel(
+                id=row[0], name=row[1], width=row[2], height=row[3], url=row[4], url_resize=row[5], date_added=row[6], date_identified=row[7]
+            )
+        )
+
+    cur.close()
+    conn.close()
+    return formatted_photos
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+    return {"message": "API Root. Welcome to FastAPI!"}
 
 
 @app.get("/path")
