808aaf9605418fb13db37423f151aa7a65b3e56a
quickstark <dirk@quickstark.com>
2022-11-01 15:56:19 -0500

    Update to CORs policy

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ origins = [
 ]
 
 app = FastAPI(debug=True)
-app.add_middleware(CORSMiddleware, allow_origins=origins,
-                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=["*"],
+                   allow_credentials=False, allow_methods=["*"], allow_headers=["*"])
 
 # define Python user-defined exceptions
 
