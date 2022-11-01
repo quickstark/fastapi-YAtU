91ab58dc408f3fb4896c4d1085f5bc0b2f555e43
quickstark <dirk@quickstark.com>
2022-11-01 15:31:22 -0500

    Added CORs support

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
-from datetime import date
-from typing import List, Optional
+import imp
 import os
-from dotenv import load_dotenv
+from datetime import date
+from re import S
+from typing import List, Optional
+
+import boto3
 import psycopg2
-from fastapi import FastAPI
+import sentry_sdk
+# Configure the client
+from botocore.config import Config
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from sentry_sdk import capture_exception
 
-app = FastAPI(debug=True)
-
+# Prep our environment variables / upload .env to Railway.app
+# (or create manually in the FastAPI Railway settings)
 load_dotenv()
 DB = os.getenv('PGDATABASE')
-HOST= os.getenv('PGHOST')
+HOST = os.getenv('PGHOST')
 PORT = os.getenv('PGPORT')
 USER = os.getenv('PGUSER')
 PW = os.getenv('PGPASSWORD')
+AWS_KEY = os.getenv('AMAZON_KEY_ID')
+AWS_SECRET = os.getenv('AMAZON_KEY_SECRET')
+AWS_BUCKET = os.getenv('AMAZON_S3_BUCKET')
+
+# Instantiate the Sentry SDK
+SENTRY_DSN = os.getenv('FAST_SENTRY_DSN')
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    traces_sample_rate=1.0,
+)
+
+# Instantiate an AWS Session (orthogonal to Client and Resource)
+AWS_SESSION = boto3.Session(
+    region_name="us-east-2",
+    aws_access_key_id=AWS_KEY,
+    aws_secret_access_key=AWS_SECRET
+)
+
+app = FastAPI(debug=True)
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+# define Python user-defined exceptions
+class SentryError(Exception):
+    """Base class for custom Sentry exceptions"""
+    pass
+
 
 class Msg(BaseModel):
     msg: str
 
+# Extend an ImageModel from pydantic BaseModel
 class ImageModel(BaseModel):
     id: int
     name: str
@@ -27,28 +64,101 @@ class ImageModel(BaseModel):
     url_resize: Optional[str]
     date_added: Optional[date]
     date_identified: Optional[date]
+    ai_labels: Optional[list]
+    ai_text: Optional[list]
 
 @app.get("/images", response_model=List[ImageModel])
 async def get_all_images():
-    #Connect to Postgres
+
+    # Connect to Postgres
     conn = psycopg2.connect(
         database=DB, user=USER, password=PW, host=HOST, port=PORT
     )
     cur = conn.cursor()
     cur.execute("SELECT * FROM images ORDER BY id DESC")
-    rows = cur.fetchall()
+    images = cur.fetchall()
 
     formatted_photos = []
-    for row in rows:
+    for image in images:
         formatted_photos.append(
             ImageModel(
-                id=row[0], name=row[1], width=row[2], height=row[3], url=row[4], url_resize=row[5], date_added=row[6], date_identified=row[7]
+                id=image[0], name=image[1], width=image[2], height=image[3], url=image[4], url_resize=image[
+                    5], date_added=image[6], date_identified=image[7], ai_labels=image[8], ai_text=image[9]
             )
         )
 
     cur.close()
     conn.close()
     return formatted_photos
+
+
+@app.post("/add_image", status_code=201)
+async def add_photo(file: UploadFile):
+    print(f"Upload File Endpoint ${file.filename} - ${file.content_type}")
+
+    # Create an S3 Client from our authenticated AWS Session
+    awsclient = AWS_SESSION.resource("s3",)
+    bucket = awsclient.Bucket(AWS_BUCKET)
+    bucket.upload_fileobj(file.file, file.filename)
+
+    uploaded_file_url = f"https://{AWS_BUCKET}.s3.amazonaws.com/{file.filename}"
+
+    amzlabels, amztext, amzmods = amazon_detection(file)
+    
+    # Case insensitive check to see if the image contained the word "error"
+    if any('Error'.casefold() or 'Errors'.casefold() in text.casefold() for text in amztext):
+        print("Yes, we have an error")
+        try:
+            error_message = f"Image Error - {' '.join(amztext)}"
+            raise Exception(error_message)
+        except Exception as err:
+            capture_exception(err)
+            print(err)
+
+    # Store the returned S3 URL in Postgres
+    conn = psycopg2.connect(
+        database=DB, user=USER, password=PW, host=HOST, port=PORT
+    )
+    cur = conn.cursor()
+    # Note: don't be tempted to use string interpolation on the SQL string ...
+    # have never gotten that to accept a List into a text[] or varchar[] Postgres column
+    SQL = "INSERT INTO images (name, url, ai_labels, ai_text) VALUES (%s, %s, %s, %s)"
+    DATA = (file.filename, uploaded_file_url, amzlabels, amztext)
+    cur.execute(SQL, DATA)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+@app.delete("/delete_image/{id}", status_code=201)
+async def delete_photo(id):
+    print(f"Delete File {id}")
+
+    # Delete the associated file information from Postgres
+    conn = psycopg2.connect(
+        database=DB, user=USER, password=PW, host=HOST, port=PORT
+    )
+
+    # Note: don't be tempted to use string interpolation on the SQL string ...
+    # have never gotten that to accept a List into a text[] or varchar[] Postgres column
+    SQL = "SELECT * FROM images WHERE id = %s"
+    DATA = (id,)
+    cur = conn.cursor()
+    cur.execute(SQL, DATA)
+    image = cur.fetchone()
+    print(f"Fetched Image Postgres: {image}")
+
+    SQL = "DELETE FROM images WHERE id = %s"
+    cur.execute(SQL, DATA)
+    print(f"Deleted Image from Postgres")
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    # Create an S3 Client from our authenticated AWS Session
+    # Use to delete our S3 file using the filename
+    awsclient = AWS_SESSION.client("s3",)
+    response = awsclient.delete_object(Bucket=AWS_BUCKET, Key=image[1])
+    print(f"Amazon Deletion {response}")
 
 @app.get("/")
 async def root():
@@ -68,3 +178,33 @@ async def demo_post(inp: Msg):
 @app.get("/path/{path_id}")
 async def demo_get_path_id(path_id: int):
     return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+def amazon_detection(file):
+    awsclient = AWS_SESSION.client("rekognition")
+    detect_labels_res = awsclient.detect_labels(
+        Image={'S3Object': {'Bucket': AWS_BUCKET, 'Name': file.filename}})
+    detect_text_res = awsclient.detect_text(
+        Image={'S3Object': {'Bucket': AWS_BUCKET, 'Name': file.filename}})
+    detect_moderation_res = awsclient.detect_moderation_labels(
+        Image={'S3Object': {'Bucket': AWS_BUCKET, 'Name': file.filename}})
+
+    detect_moderation_list = []
+    for label in detect_moderation_res["ModerationLabels"]:
+        if label["Confidence"] > 50:
+            detect_moderation_list.append(label["Name"])
+
+    detect_labels_list = []
+    for label in detect_labels_res["Labels"]:
+        if label["Confidence"] > 80:
+            detect_labels_list.append(label["Name"])
+
+    detect_text_list = []
+    for text in detect_text_res['TextDetections']:
+        if text["Type"] == "LINE" and text["Confidence"] > 80:
+            detect_text_list.append(text["DetectedText"])
+
+    print(detect_labels_list)
+    print(detect_text_list)
+    print(detect_moderation_list)
+    
+    return detect_labels_list, detect_text_list, detect_moderation_list
