f0a9aae3ed63f0cf28c5ace7765eec0e6f33657e
quickstark <dirk@quickstark.com>
2024-03-13 13:13:23 -0500

    #patch Add Codecov workflow and tests

--- tests/mongo_test.py
+++ tests/mongo_test.py
@@ -1,19 +1,19 @@
-import os 
-print(os.getcwd())
-
+import os
 from unittest.mock import AsyncMock
 
 import pytest
 
 from src.mongo import *
 
+print(os.getcwd())
 
 
 @pytest.mark.asyncio
 async def test_get_one_mongo(monkeypatch):
-   
-    # Call the async function and await its result
-    result = await get_one_mongo("648b7444769c327f2a7cf0fe")
 
-    # Assert that the result matches what we expect
-    assert result == None
+    # Call the async function and await its result
+    response = await get_one_mongo("648b7444769c327f2a7cf0fe")
+
+    assert isinstance(response, dict)
+    expected_keys = {"name", "url", "ai_labels", "id"}
+    assert expected_keys.issubset(response.keys())
